e7b338d73a15645e88d41ce1a2188f8bf4ef80ca
Luis Vilanova <luisvilanovarodriguez@gmail.com>
2018-03-04 22:31:22 +0100

    check done

--- src/jsx/final/TodoItem.js
+++ src/jsx/final/TodoItem.js
@@ -8,19 +8,19 @@ import TodoButton from './TodoButton';
 class TodoItem extends JSXComponent {
 	render() {
 		let elementClasses = `todo-item${this.props.todo.done ? ' todo-item-done' : ''}`;
-
+		let checkClass = `${this.props.todo.done ? ' fas fa-check-circle' : 'far fa-check-circle'}`;
 		return [
 			<button
 				data-onclick={this.handleClick.bind(this)}
 				>
-					<i class="fa fa-check-circle"></i>
+					<i 
+						class={checkClass}>
+					</i>
 				</button>,
 			<li
 				class={elementClasses}
 			>
 				{this.props.todo.title}
-				
-			
 			</li>
 				
 		];
@@ -31,6 +31,7 @@ class TodoItem extends JSXComponent {
 			index: this.props.index
 		});
 	}
+	
 }
 
 
